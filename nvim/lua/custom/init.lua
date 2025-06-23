871e29d0e12b144e4975956b20f60c77c5ae242d
Dominik Deutsch <dominik.deutsch@proton.me>
2025-06-23 22:07:31 +0200

    Added telescope

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,1 +1,8 @@
+-- file explorer
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
